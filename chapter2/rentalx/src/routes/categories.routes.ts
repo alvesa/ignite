76af15cc28884acc6636cfe165ecb38eaf8dde36
Andre Alves <andresistinfo@gmail.com>
2023-01-25 22:47:18 +0000

    Adjusment of linter and addition of new service

--- chapter2/rentalx/src/routes/categories.routes.ts
+++ chapter2/rentalx/src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
+import { CreateCategoryService } from '../modules/cars/services/CreateCategoryService';
 
 const categoryRepository = new CategoriesRepository();
 
@@ -12,12 +12,12 @@ const categoryAlreadyExists = (req, res, next) => {
   const categogoryAlreadyExists = categoryRepository.findByName(name);
 
   if (categogoryAlreadyExists)
-    return res.status(400).json({ message: "Category already exists" });
+    return res.status(400).json({ message: 'Category already exists' });
 
   return next();
 };
 
-categoriesRoutes.post("", categoryAlreadyExists, (request, response) => {
+categoriesRoutes.post('', categoryAlreadyExists, (request, response) => {
   const { name, description } = request.body;
 
   const createCategoryService = new CreateCategoryService(categoryRepository);
@@ -27,8 +27,9 @@ categoriesRoutes.post("", categoryAlreadyExists, (request, response) => {
   return response.status(201).json();
 });
 
-categoriesRoutes.get("", (request, response) => {
-  return response.status(200).json(categoryRepository.getAll());
+categoriesRoutes.get('', (request, response) => {
+  return response.status(200).json(categoryRepository.list());
 });
 
 export { categoriesRoutes };
+
--- chapter2/rentalx/src/server.ts
+++ chapter2/rentalx/src/server.ts
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express';
 
-import { categoriesRoutes } from "./routes/categories.routes";
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 const PORT = 3333;
 
 app.use(express.json());
-app.use("/categories", categoriesRoutes);
+app.use('/categories', categoriesRoutes);
 
 app.listen(PORT, () => console.log(`running on port ${PORT} ...`));
