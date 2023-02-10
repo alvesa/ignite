62238d4b5967bcc898aef3ac568d7e2da5ef63d8
Andre Alves <andresistinfo@gmail.com>
2023-02-10 06:23:30 +0000

    Inserted the user endpoint implementation

---
+++ rentalx/src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,27 @@
+import { inject, injectable } from 'tsyringe';
+
+import { ICreateUserDTO } from '../../dtos/ICreateUserDTO';
+import { IUserRepository } from '../../repositories/IUserRepository';
+
+@injectable()
+export class CreateUserUseCase {
+  constructor(
+    @inject('UsersRepository') private usersRepository: IUserRepository
+  ) {}
+
+  async execute({
+    name,
+    username,
+    email,
+    password,
+    driver_license,
+  }: ICreateUserDTO) {
+    await this.usersRepository.create({
+      name,
+      username,
+      email,
+      password,
+      driver_license,
+    });
+  }
+}
--- rentalx/src/shared/container/index.ts
+++ rentalx/src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '../../modules/accounts/repositories/implementations/UsersRepository';
+import { IUserRepository } from '../../modules/accounts/repositories/IUserRepository';
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
 import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationRepository';
@@ -13,4 +15,9 @@ container.registerSingleton<ICategoriesRepository>(
 container.registerSingleton<ISpecificationRepository>(
   'SpecificationRepository',
   SpecificationRepository
+);
+
+container.registerSingleton<IUserRepository>(
+  'UsersRepository',
+  UsersRepository
 );
