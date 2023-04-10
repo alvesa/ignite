3c7db95471375b71a7c544c8d989ac18071e2588
Andre Alves <andresistinfo@gmail.com>
2023-04-10 08:20:39 +0100

    Added the local db options and removed the jest report from tests

--- rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 
 import { app } from '@shared/infra/http/app';
 
-describe('Create Category Controller', async () => {
+describe('Create Category Controller', () => {
   it('should be able to create a new category', async () => {
     const response = await request(app)
       .post('/categories')
