af6e3b2b7fdec8ec4c72718fff46572b5999eefb
Andre Alves <andresistinfo@gmail.com>
2023-04-10 09:17:11 +0100

    Fixing the integration test admin  password creation

--- rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -8,7 +8,7 @@ import { app } from '@shared/infra/http/app';
 let connection: Connection;
 const adminEmail = 'admin@email.com';
 
-describe('Create Category Controller', () => {
+describe('List Category Controller', () => {
   beforeAll(async () => {
     connection = await createConnection();
     await connection.runMigrations();
@@ -26,13 +26,14 @@ describe('Create Category Controller', () => {
     await connection.close();
   });
 
-  it('should be able to create a new category', async () => {
+  it('should be able to list all categories', async () => {
     const responseToken = await request(app)
       .post('/sessions')
       .send({ email: adminEmail, password: 'admin' });
 
     const { token } = responseToken.body;
-    const response = await request(app)
+
+    await request(app)
       .post('/categories')
       .send({
         name: 'Category supertest',
@@ -42,26 +43,9 @@ describe('Create Category Controller', () => {
         Authorization: `Bearer ${token}`,
       });
 
-    expect(response.status).toBe(201);
-  });
+    const categories = await request(app).get('/categories');
 
-  it('should not be able to create a new category with existing name already created', async () => {
-    const password = hash('admin', 8);
-    const responseToken = await request(app)
-      .post('/sessions')
-      .send({ email: adminEmail, password });
-
-    const { token } = responseToken.body;
-    const response = await request(app)
-      .post('/categories')
-      .send({
-        name: 'Category supertest',
-        description: 'Category supertest',
-      })
-      .set({
-        Authorization: `Bearer ${token}`,
-      });
-
-    expect(response.status).toBe(400);
+    expect(categories.status).toBe(200);
+    expect(categories.body.length).toBe(1);
   });
 });
