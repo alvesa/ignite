48e10494122707fbadd36594ab35526d2bac2453
Andre Alves <andresistinfo@gmail.com>
2023-07-11 07:04:16 +0100

    Adding lint

--- project-fastfy/src/index.ts
+++
@@ -1,9 +0,0 @@
-import fastify from 'fastify';
-
-const app = fastify()
-
-app.get('/hello', () => {
-  return 'Hello Worlds!'
-})
-
-app.listen(3333).then(() => console.log('HTTP server running'));
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import fastify from 'fastify'
+
+const app = fastify()
+
+app.get('/hello', () => {
+  return 'Hello Worlds!'
+})
+
+app.listen(3333).then(() => console.log('HTTP server running'))
