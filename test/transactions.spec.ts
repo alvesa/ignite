05c8e7cd419b1769e6c80a67bfee4165119d7bcf
ANDRE ALVES <andrealves@MacBook-Pro-de-ANDRE.local>
2023-07-27 06:44:56 +0100

    Added selection transactions test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,6 @@
 import request from 'supertest'
 import { app } from '../src/app'
-import { afterAll, beforeAll, describe, it } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
 describe('Transactions routes', () => {
   beforeAll(async () => {
@@ -20,5 +20,27 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({ amount: 5000, title: 'New transaction' }),
+    ])
   })
 })
