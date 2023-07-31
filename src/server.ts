b747e9d0bbea5566719aab67cb9f48efd9e93747
ANDRE ALVES <andrealves@MBP-de-ANDRE.lan>
2023-07-31 08:59:13 +0100

    Included dynamic ports for env and installed postgress client

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import { app } from '../src/app'
 import { env } from './env'
 
-app.listen(env.PORT).then(() => console.log('HTTP server running'))
+app
+  .listen(env.PORT)
+  .then(() => console.log(`HTTP server running on port ${env.PORT}`))
